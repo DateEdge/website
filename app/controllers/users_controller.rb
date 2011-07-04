0453acb4362bee6b45ba49da30536f022bff3d52
Shane Becker <veganstraightedge@gmail.com>
2011-07-04 15:22:09 -0700

    vanity urls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.where(:username => params[:username]).first
 
     unless @user.age_appropiate?(current_user)
-      redirect_to users_path
+      redirect_to people_path
     end
   end
 
@@ -17,14 +17,13 @@ class UsersController < ApplicationController
 
   def update
     params[:user][:your_labels_attributes].delete_if {|k, v| v['label_id'] == "0"}
-    @user = User.find(params[:id])
 
-    if @user.username
+    if current_user.username
       params[:user].delete(:username)
     end
 
-    if @user.update_attributes(params[:user])
-      redirect_to(@user, :notice => 'User was successfully updated.')
+    if current_user.update_attributes(params[:user])
+      redirect_to(person_path(current_user.username), :notice => 'User was successfully updated.')
     else
       render :action => "edit"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Dxe::Application.routes.draw do
   get "welcome/index"
   root :to => "welcome#index"
 
-  resources :users, :except => [:new, :create]
-
-  match "/settings" => "users#edit", :as => :settings
+  resources :users, :only => [:update]
 
   match "/signout" => "sessions#destroy", :as => :signout
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure" => redirect("/")
+
+  match "/settings"  => "users#edit",   :as => :settings
+  match "/people"    => "users#index",  :as => :people
+  match "/:username" => "users#show",   :as => :person
 end
