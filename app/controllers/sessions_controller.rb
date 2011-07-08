6518af6d1309462e19b5327168f67f826b5c8e13
Bookis Smuin <vegan.bookis@gmail.com>
2011-07-08 00:03:06 -0700

    private method, my ass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,7 +20,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
     end
 
-    redirect_to start_url, :notice => "Signed in!"
+    redirect_to people_path, :notice => "Signed in!"
   end
 
   def destroy
