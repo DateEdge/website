f6560b2777e1b5f31f188fbb7c6dba491d1959fc
Bookis Smuin <vegan.bookis@gmail.com>
2011-07-12 14:07:38 -0700

    handle redirects on unsavory users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,13 @@ class ApplicationController < ActionController::Base
   before_filter :restrict_non_visible_user
   helper_method :current_user
   helper_method :unread_count
+  helper_method :redirect_age_inappropriate
 
   private
+  
+  def redirect_if_age_inappropriate(user)
+    redirect_to people_path if user.nil? || user.age_inappropiate?(current_user)
+  end
 
   def restrict_non_visible_user
     if current_user && !current_user.visible?
