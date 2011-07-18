a7d19f432114ed2355f22386d67b85ae66fc9e44
Bookis Smuin <vegan.bookis@gmail.com>
2011-07-17 22:35:47 -0700

    make thumbnails

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,15 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
-  
+  before_create :clean_url
   after_save :check_avatar, :if => :avatar?
 
   private
+  
+  def clean_url
+    # FIX ME
+    @remote_image_url = self.remote_image_url.gsub("+", "%20")
+  end
   
   def check_avatar
     avatars = self.user.photos.where(:avatar => true).all
