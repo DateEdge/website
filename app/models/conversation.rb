2f37fa2da1ae5d36e5e037e394b324ed4fb09713
Shane Becker <veganstraightedge@gmail.com>
2011-11-20 15:54:44 -0800

    fixed unread count to not include counterpart's unread status

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,7 +3,7 @@ class Conversation < ActiveRecord::Base
   default_scope joins(:messages).order('messages.updated_at asc')
   has_many :messages, :dependent => :destroy
 
-  belongs_to :sender,     :foreign_key => :user_id,       :class_name => "User"
+  belongs_to :sender,    :foreign_key => :user_id,      :class_name => "User"
   belongs_to :recipient, :foreign_key => :recipient_id, :class_name => "User"
 
   def participants
@@ -19,13 +19,13 @@ class Conversation < ActiveRecord::Base
   def unread(user)
     messages.where(:recipient_id => user.id).unread
   end
-  
+
   def unread?(user)
     unread(user).any?
   end
 
   def self.unread_messages(user)
-    user.conversations.all.uniq.map { |convo| convo.messages.unread.count }.sum
+    user.conversations.all.uniq.map { |convo| convo.messages.received(user).unread.count }.sum
   end
-  
+
 end
