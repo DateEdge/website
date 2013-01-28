fab0db4feb4750fd5aecf3dd8c2aa6f1126a08d7
Shane Becker <veganstraightedge@gmail.com>
2013-01-27 23:19:13 -0800

    version bumped rails to 3.2.11

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Dxe::Application.config.session_store :cookie_store, :key => '_dxe_session'
+Dxe::Application.config.session_store :cookie_store, key: '_dxe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dxe::Application.config.session_store :active_record_store
