a9c138de88467c3ed43dcb4d9d8cb6b78bad0e82
Shane Becker <veganstraightedge@gmail.com>
2013-06-13 02:35:03 -0700

    changed people routes to /@username

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Dxe::Application.routes.draw do
 
   root :to => "welcome#index"
 
-  resources :users, :only => [:update, :destroy]
+  resources :users,   :only => [:update, :destroy]
   resources :photos
   resources :crushes, :only => [:create, :destroy]
 
@@ -13,9 +13,9 @@ Dxe::Application.routes.draw do
   match "/auth/:provider/callback" => "sessions#create"
   match "/auth/failure"            => "sessions#failure"
 
-  match "/start"     => "users#new",    :as => :start
-  match "/oops"      => 'users#create', :as => :user_create
-  match "/settings"  => "users#edit",   :as => :settings
-  match "/people"    => "users#index",  :as => :people
-  match "/:username" => "users#show",   :as => :person
+  match "/start"      => "users#new",    :as => :start
+  match "/oops"       => 'users#create', :as => :user_create
+  match "/settings"   => "users#edit",   :as => :settings
+  match "/people"     => "users#index",  :as => :people
+  match "/@:username" => "users#show",  :as => :person
 end
