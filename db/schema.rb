291fb9e4af2717b789d43bf1daf8b5231d8db3ac
Bookis <vegan.bookis@gmail.com>
2013-07-03 11:18:04 -0700

    fix some specs

--- db/schema.rb
+++ db/schema.rb
@@ -9,80 +9,80 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110705054514) do
+ActiveRecord::Schema.define(version: 20110705054514) do
 
-  create_table "conversations", :force => true do |t|
+  create_table "conversations", force: true do |t|
     t.integer  "user_id"
     t.integer  "recipient_id"
     t.string   "subject"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table "countries", :force => true do |t|
+  create_table "countries", force: true do |t|
     t.string   "name"
     t.string   "abbreviation"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table "crushes", :force => true do |t|
+  create_table "crushes", force: true do |t|
     t.integer  "crusher_id"
     t.integer  "crushee_id"
-    t.boolean  "secret",     :default => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.boolean  "secret",     default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
-  create_table "diets", :force => true do |t|
+  create_table "diets", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "labels", :force => true do |t|
+  create_table "labels", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "messages", :force => true do |t|
+  create_table "messages", force: true do |t|
     t.integer  "conversation_id"
     t.integer  "sender_id"
     t.integer  "recipient_id"
     t.text     "body"
-    t.boolean  "unread",          :default => true
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.boolean  "unread",          default: true
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  create_table "photos", :force => true do |t|
+  create_table "photos", force: true do |t|
     t.string   "image"
     t.boolean  "avatar"
     t.text     "caption"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "providers", :force => true do |t|
+  create_table "providers", force: true do |t|
     t.string   "name"
     t.string   "uid"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "states", :force => true do |t|
+  create_table "states", force: true do |t|
     t.string   "name"
     t.string   "abbreviation"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.date     "birthday"
     t.string   "name"
     t.string   "username"
@@ -93,21 +93,21 @@ ActiveRecord::Schema.define(:version => 20110705054514) do
     t.string   "me_gender_map"
     t.string   "you_gender"
     t.string   "you_gender_map"
-    t.boolean  "visible",        :default => false
+    t.boolean  "visible",        default: false
     t.text     "bio"
     t.integer  "label_id"
     t.integer  "state_id"
     t.integer  "country_id"
     t.integer  "diet_id"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
   end
 
-  create_table "your_labels", :force => true do |t|
+  create_table "your_labels", force: true do |t|
     t.integer  "user_id"
     t.integer  "label_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe MessagesController do
-
+  let(:user) { User.create(:username => "Shane", :name => "SB", :email => "test@example.com", :birthday => 25.years.ago, visible: true) }
+  let(:user2) { User.create(:username => "Bookis", :name => "BS", :email => "bs@example.com", :birthday => 25.years.ago, visible: true) }
+  let(:conversation) { Conversation.create }
+  
+  before do 
+    DatabaseCleaner.clean
+    session[:user_id] = user.id 
+    Conversation.stub(:find) { conversation }
+    Conversation.any_instance.stub(:counterpart).with(user) { user2 }
+    User.stub(:where) { user2 }
+  end
+  
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -9,10 +20,10 @@ describe MessagesController do
     end
   end
 
-  describe "GET 'create'" do
+  describe "POST 'create'" do
     it "should be successful" do
-      get 'create'
-      response.should be_success
+      post 'create', message: {body: "Body", subject: "Subject", receipient_id: user2.id, conversation_id: conversation.id}
+      response.should redirect_to conversation_path(conversation.id)
     end
   end
 
