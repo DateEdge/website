b054901f1e94ac58eb01fb125c2bdff8867d569a
Shane Becker <veganstraightedge@gmail.com>
2013-07-21 14:38:59 -0700

    added terms title

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -2,5 +2,6 @@ class AboutController < ApplicationController
   skip_before_filter :restrict_non_visible_user, :only => [:terms]
   
   def terms
+    @title = "Terms &amp; Conditions on Date Edge"
   end
 end
