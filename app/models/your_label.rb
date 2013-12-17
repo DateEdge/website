1a056aa9b18521ad778f17275e23d582c33d6724
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-17 11:28:30 -0800

    remove db calls from user edit view

--- app/models/your_label.rb
+++ app/models/your_label.rb
@@ -4,4 +4,8 @@ class YourLabel < ActiveRecord::Base
   validates  :user,  presence: true
   validates  :label, presence: true
   validates  :label_id, uniqueness: { scope: :user_id }
+  
+  def self.label_assignments
+    Hash[select(:id, :label_id).collect {|label| [label.label_id, label.id] }]
+  end
 end
