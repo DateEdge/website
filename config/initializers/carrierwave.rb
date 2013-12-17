ed0ef23aa26e59e4ce9eb743479974375000bb85
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-17 14:11:58 -0800

    changes carrierwave s3 settings to use host and ENV vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAJJU6W55ZCDRCHEDQ',
-    aws_secret_access_key: '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',
+    aws_access_key_id:     ENV["DXE_AWS_ACCESS_KEY"],
+    aws_secret_access_key: ENV["DXE_AWS_SECRET_KEY"],
     region:                'us-west-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = "dxe-#{Rails.env}"
-  # config.fog_host       = 'https://assets.example.com'
+  config.fog_directory  = "assets.dateedge.com"
+  config.asset_host       = 'http://assets.dateedge.com'
   # config.fog_public     = false
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
