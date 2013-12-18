5c0c503bb394ca97e65ae6a5193701d83d8744b6
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-17 17:27:39 -0800

    sorts photos by created at

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
   has_many :inbound_messages, class_name: "Message", foreign_key: :recipient_id
   
   has_many :providers, dependent: :destroy
-  has_many :photos,    dependent: :destroy
+  has_many :photos, -> { order "created_at desc" }, dependent: :destroy
   has_many :your_labels
   has_many :desired_labels, -> { uniq }, through: :your_labels, source: :label, as: :label, source_type: "Label"
   has_many :desired_diets,  -> { uniq }, through: :your_labels, source: :label, as: :label, source_type: "Diet"
