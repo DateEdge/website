2bcadfc3dfe26e2e907be7594d3065c86e97bac9
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-17 18:25:43 -0800

    try to fix s3 creds for creating images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,11 +10,11 @@ else
       provider:              'AWS',
       aws_access_key_id:     ENV["DXE_AWS_ACCESS_KEY"],
       aws_secret_access_key: ENV["DXE_AWS_SECRET_KEY"],
-      region:                'us-west-1'  # optional, defaults to 'us-east-1'
+      endpoint:              "https://assets.dateedge.com:8080"
     }
-    if Rails.env.production?
+    unless Rails.env.production?
       config.fog_directory  = "assets.dateedge.com"
-      config.asset_host     = 'http://assets.dateedge.com'
+      config.asset_host     = 'https://assets.dateedge.com'
     else
       config.fog_directory  = "dxe-#{Rails.env}"
     end
