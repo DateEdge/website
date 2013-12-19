d4e72f0c1bc3c9f2c9e7cfa214c6509463ddf35d
Shane Becker <veganstraightedge@gmail.com>
2013-12-18 18:10:10 -0800

    get route for /oops in case someone refreshes on the start error /oops page

--- config/routes.rb
+++ config/routes.rb
@@ -10,15 +10,16 @@ Dxe::Application.routes.draw do
   resources :messages,      only: [:create, :new]
 
   get "/terms",                   to: "about#terms",      as: :terms
-  
+
   get "/signout",                 to: "sessions#destroy", as: :signout
   get "/auth/:provider/callback", to: "sessions#create"
   get "/auth/failure",            to: "sessions#failure"
 
+  get   "/oops",                  to: redirect("/start")
+  patch "/oops",                  to: 'users#create', as: :user_create
   get   "/start",                 to: "users#new",    as: :start
-  patch  "/oops",                 to: 'users#create', as: :user_create
   get   "/settings",              to: "users#edit",   as: :settings
   patch "/settings",              to: "users#update", as: :update_settings
   get   "/people",                to: "users#index",  as: :people
-  get   "/@:username",            to: "users#show",   as: :person, username: /[^\/]+/ 
+  get   "/@:username",            to: "users#show",   as: :person, username: /[^\/]+/
 end
