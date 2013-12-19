c66dd630870c4c340200daa97e6b89006912e745
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-18 16:01:03 -0800

    changes conversations subject to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131217205106) do
+ActiveRecord::Schema.define(version: 20131218235836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20131217205106) do
   create_table "conversations", force: true do |t|
     t.integer  "user_id"
     t.integer  "recipient_id"
-    t.string   "subject"
+    t.text     "subject"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
