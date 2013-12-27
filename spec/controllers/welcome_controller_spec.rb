d9a3313a38c6efb16e2b42ea122e4025e05836ef
Bookis Smuin <bookis.smuin@gmail.com>
2013-12-26 16:35:56 -0800

    writes specs for showing blocked users

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe WelcomeController do
-
+  let!(:user) { create(:user) }
+  
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
@@ -9,15 +10,40 @@ describe WelcomeController do
     end
     
     describe "logged in" do
-      let(:user) { create(:user) }
       before { session[:user_id] = user.id }
       it "calls viewable users on current user" do
         controller.stub(:current_user) { user }
         user.should_receive(:viewable_users).once.and_return(User.all)
         get :index
       end
+      
+      describe "blocks" do
+        let!(:bookis) { create(:bookis) }
+      
+        it "shows bookis by default" do
+          get :index
+          expect(assigns(:users)).to include bookis
+        end
+      
+        it "does not show bookis" do
+          create(:block, blocker_id: user.id, blocked_id: bookis.id)
+          get :index
+          expect(assigns(:users)).to_not include bookis
+        end
+
+        it "does not show shane if bookis is logged in" do
+          session[:user_id] = bookis.id
+          create(:block, blocker_id: user.id, blocked_id: bookis.id)
+          get :index
+          expect(assigns(:users)).to_not include user
+        end
+      
+        it "includes user by default" do
+          session[:user_id] = bookis.id
+          get :index
+          expect(assigns(:users)).to include user
+        end
+      end
     end
   end
-  
-
 end
