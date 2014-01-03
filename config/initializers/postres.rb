0effa246164a9f25ef59c63286f0be1835e68278
Bookis Smuin <bookis.smuin@gmail.com>
2014-01-03 00:28:58 -0800

    removes postgres changes

--- config/initializers/postres.rb
+++ config/initializers/postres.rb
@@ -1,3 +1,3 @@
-ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.tap do |klass|
-  klass::OID.register_type('hstore', klass::OID::Identity.new)
-end
+# ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.tap do |klass|
+#   klass::OID.register_type('hstore', klass::OID::Identity.new)
+# end
