b734966493b46706cad73a5212895e32cceb8a67
Bookis Smuin <bookis.smuin@gmail.com>
2014-02-27 20:08:12 -0800

    fixes search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   skip_before_filter :restrict_non_visible_user, only: [:new, :create]
   before_action :require_login, only: [:edit, :update, :destroy]
+  before_action :search_term, only: :index
   def index
     @title = "People Using Date Edge"
     @slug  = "people"
