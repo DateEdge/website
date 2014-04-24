2228a5ba930ef56e15215dd69e8d7fa9162a9ada
Shane Becker <veganstraightedge@gmail.com>
2014-04-24 15:45:09 -0700

    removed searches#show view and rendered users#index view from searches#show controller action (since the two views were now identical duplicates)

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
 class SearchesController < ApplicationController
   before_action :require_login, only: [:index]
   before_action :search_term, only: :show
-  
+
   def index
     @title_text = params[:column].titleize
     @groups     = current_user.viewable_users.group_by_column(params[:column].to_sym)
     @total      = @groups.sum(&:last)
   end
-  
+
   def show
     @title = "People Using Date Edge"
     @slug  = "people"
@@ -18,18 +18,22 @@ class SearchesController < ApplicationController
     end
     @total = @users.count
     @users = @users.order('created_at desc').paginate(page: params[:page] ||= 1)
+
+    render "/users/index"
   end
-  
+
   private
-  
+
   def search_term
     return unless params[:search]
+
     search = params[:search].split("/", 2)
     @search, @query, @column = if search.count.even?
       [Hash[*search], search.last, search.first]
     else
       [search.first, search.first, nil]
     end
+
     @column = nil unless User::ALLOWED_SEARCH_COLUMNS.include? @column
   end
 end
