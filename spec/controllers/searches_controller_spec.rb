2533b709afd3b8fc894718c2863fe9ac82c25320
Bookis Smuin <bookis.smuin@gmail.com>
2014-04-26 23:09:09 -0700

    moves search route to users controller. Finishes #101

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -12,21 +12,6 @@ describe SearchesController do
       response.should be_success
     end
     
-    it "shows everyone" do
-      get :show, search: "bookis"
-      expect(assigns(:users)).to include bookis
-    end
-
-    it "doesn't show blocked people" do
-      shane.blocks << Block.create(blocked_id: bookis.id)
-      get :show, search: "bookis"
-      expect(assigns(:users)).to_not include bookis
-    end
-    
-    it "by username field" do
-      User.should_receive(:search).with("username" => "veganstraightedge").once.and_return User.visible
-      get :show, search: "username/veganstraightedge"
-    end
   end
 
 end
