37b0b7c471c899ad2de7075d18d12a0ceaf75fc9
Shane Becker <veganstraightedge@gmail.com>
2014-04-27 21:15:06 -0700

    fixed 'desired' links in profile header. updates #113

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,8 +9,8 @@ module UsersHelper
   end
 
   def labels_list(user, type)
-    unless user.send("desired_#{type}s").blank?
-      user.send("desired_#{type}s").map{ |dd|
+    unless user.send("desired_#{type}").blank?
+      user.send("desired_#{type}").map{ |dd|
         "<b>#{ link_to dd.name.downcase, search_path(search: [type, dd.name]) }</b>"
       }.join("<span style='color:#999'>/</span>").html_safe
     end
