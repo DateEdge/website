1e1d0530cd9eda6f0775de541f88cbe10ce158eb
Bookis Smuin <bookis.smuin@gmail.com>
2014-04-30 16:42:08 -0700

    hides flag buttons if logged out

--- app/helpers/red_flags_helper.rb
+++ app/helpers/red_flags_helper.rb
@@ -1,5 +1,6 @@
 module RedFlagsHelper
   def flagged?(obj)
+    return unless current_user
     current_user.red_flag_reports.where(flaggable_id: obj.id, flaggable_type: obj.class.to_s).any?
   end
 end
