1ec946fc53f74ca86ecc425d16c995171dea6813
Bookis Smuin <bookis.smuin@gmail.com>
2014-04-30 11:45:52 -0700

    refactors flags controller

--- app/controllers/red_flags_controller.rb
+++ app/controllers/red_flags_controller.rb
@@ -2,11 +2,7 @@ class RedFlagsController < ApplicationController
   
   def create
     flag = current_user.red_flag_reports.new(flaggable_id: params[:id], flaggable_type: params.require(:red_flag)[:flaggable_type])
-    flash[:notice] = if flag.save
-      "Flagged!"
-    else
-      "Already flagged"
-    end
+    flash[:notice] = flag.save ? "Flagged!" : "Already flagged"
     user = flag.flaggable_type == "User" ? flag.flaggable : flag.flaggable.user
     redirect_to user
   end
