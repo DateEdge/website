e3069595b3c7c390f5fd9bfbd4fdf1edaaec6429
Bookis Smuin <bookis.smuin@gmail.com>
2014-06-18 21:33:27 -0700

    makes redflags dependent on their parent

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   attr_accessor :manipulate
   belongs_to :user, counter_cache: true
+  has_many :red_flags, as: :flaggable, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :image, presence: true
   before_create :clean_url
@@ -9,20 +10,20 @@ class Photo < ActiveRecord::Base
   after_save  :recreate_versions!, if: :manipulate
 
   private
-  
+
   def recreate_versions!
     self.image.recreate_versions!
   end
-  
+
   def process_manipulation
     return unless self.manipulate.respond_to? :has_key?
     self.image.rotate(self.manipulate)
   end
-  
+
   def clean_url
     self.remote_image_url.gsub!("+", "%20") if self.remote_image_url
   end
-  
+
   def check_avatar
     avatars = self.user.photos.where(avatar: true)
     avatars.to_a.delete(self)
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,17 +25,17 @@ end
 class User < ActiveRecord::Base
   include UserRule
   include PgSearch
-  pg_search_scope :text_search, 
-    against: {username: "A", city: "B", me_gender: "C"}, 
+  pg_search_scope :text_search,
+    against: {username: "A", city: "B", me_gender: "C"},
     using: {tsearch: {dictionary: "english", prefix: true, suffix: true}},
-    ignoring: :accents, 
+    ignoring: :accents,
     associated_against: {
       state:   [:name, :abbreviation],
       label:   [:name],
       diet:    [:name],
       country: [:name, :abbreviation]
     }
-    
+
   pg_search_scope :field_search, lambda { |query|
     field   = COLUMN_MAPPING[query.keys.first.to_sym]
     field ||= query.keys.first.to_sym
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
       { against: field, query: query.values.first }
     end
   }
-  
+
   self.per_page = 28
   store_accessor :settings, :admin, :featured, :birthday_public, :real_name_public, :email_public
 
@@ -85,9 +85,9 @@ class User < ActiveRecord::Base
   has_many :desired_straightedgeness, -> { uniq }, through: :your_labels, source: :label, as: :label, source_type: "Label"
   has_many :desired_diets,  -> { uniq }, through: :your_labels, source: :label, as: :label, source_type: "Diet"
 
-  has_many :red_flags, as: :flaggable
+  has_many :red_flags, as: :flaggable, dependent: :destroy
   has_many :red_flag_reports, class_name: "RedFlag", foreign_key: :reporter_id
-  
+
   accepts_nested_attributes_for :your_labels, allow_destroy: true, reject_if: proc { |obj| obj['label_id'] == "0" }
 
   scope :visible,       -> { where(visible: true) }
@@ -106,7 +106,7 @@ class User < ActiveRecord::Base
   validates :agreed_to_terms_at, presence: { on: :update, message: "must be agreed upon"}
 
   before_save :groom_string_fields
-  
+
   before_validation :create_canonical_username
   before_validation :generate_auth_token, on: :create
 
@@ -114,36 +114,36 @@ class User < ActiveRecord::Base
     username
   end
   class << self
-    
+
     def search(search)
       age = search.delete(:age) if search.is_a? Hash
       query = if search.is_a?(Hash) && !search.blank?
-        field_search(search) 
+        field_search(search)
       elsif !search.blank?
-        text_search(search)       
+        text_search(search)
       else
         all
       end
       query = query.age(age) if age.present?
       query
     end
-    
+
     def group_by_column(column)
       association = UserRule::ASSOCIATION_NAME[column]
       column_name = UserRule::SQL_GROUP[column]
-      
+
       group(column_name).
       where("#{column_name} IS NOT NULL AND #{column_name} != ?", "").
       includes(association).
       references(association).
       count(:id).sort_by(&:last).reverse
     end
-    
+
     def age(args)
       beginning, end_year = args.respond_to?(:each) ? [args.last.to_i, args.first.to_i] : [args.to_i, args.to_i]
       where(birthday: ((beginning.years.ago - 1.year)..end_year.years.ago))
     end
-    
+
     def create_with_omniauth(auth)
       user = send("create_for_#{auth.provider}", auth)
       user
