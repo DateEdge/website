d5bd38b79b626dd65e292ed8b059ec2b1450cbfa
Bookis Smuin <bookis.smuin@gmail.com>
2014-08-05 19:17:07 -0700

    adds #search autolinking to markdown. Closes #35

--- lib/date_edge_html.rb
+++ lib/date_edge_html.rb
@@ -1,9 +1,16 @@
 class DateEdgeHTML < Redcarpet::Render::HTML
   def postprocess(html)
-    autolink_at_usernames(html)
+    puts self.inspect
+    html = autolink_at_usernames(html)
+    autolink_searches(html)
   end
 
   def autolink_at_usernames(html)
     html.gsub(/(@[\.a-zA-Z0-9_-]+)/) {|m| "<a href='/#{$1}'>#{$1}</a>" }
   end
+
+  def autolink_searches(html)
+    html.gsub(/(#([\.a-zA-Z0-9_-]+))/) {|m| "<a href='/people?search=#{$2}'>#{$1}</a>" }
+  end
+
 end
--- spec/lib/date_edge_html_spec.rb
+++ spec/lib/date_edge_html_spec.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 
 describe DateEdgeHTML do
-  it "turns @username into a link" do
-    expect(Redcarpet::Markdown.new(DateEdgeHTML).render("@username")).to include "<a href='/@username'>@username</a>"
+  describe "@usernames" do
+    it "turns @username into a link" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("@username")).to include "<a href='/@username'>@username</a>"
+    end
+
+    it "wraps text around it" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("Hi I am @username the user")).to include "Hi I am <a href='/@username'>@username</a> the user"
+    end
+
+    it "handles ., _, -, and numbers" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("@examp1e.boy_1-mccool")).to include "<a href='/@examp1e.boy_1-mccool'>@examp1e.boy_1-mccool</a>"
+    end
   end
 
-  it "wraps text around it" do
-    expect(Redcarpet::Markdown.new(DateEdgeHTML).render("Hi I am @username the user")).to include "Hi I am <a href='/@username'>@username</a> the user"
-  end
+  describe "#searches" do
+    it "turns #search into a search link" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("I am #cool")).to include "<a href='/people?search=cool'>#cool</a>"
+    end
 
-  it "handles ., _, -, and numbers" do
-    expect(Redcarpet::Markdown.new(DateEdgeHTML).render("@examp1e.boy_1-mccool")).to include "<a href='/@examp1e.boy_1-mccool'>@examp1e.boy_1-mccool</a>"
+    it "turns #search into a search link with numbers underscores and dashes" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("I am #cool_123-guy")).to include "<a href='/people?search=cool_123-guy'>#cool_123-guy</a>"
+    end
+
+    it "starts with # is still an h1" do
+      expect(Redcarpet::Markdown.new(DateEdgeHTML).render("#Notice I am #cool")).to include "<h1>Notice"
+    end
   end
 end
