da0b95c7ff05de708bfd59a0170a43e0781acca5
Shane Becker <veganstraightedge@gmail.com>
2014-09-21 21:23:46 -0700

    Fix failing /admin spec. Updates #111.

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -8,17 +8,22 @@ describe "admin", type: :feature, js: true do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bookis)
     visit "/admin"
     expect(current_path).to eq "/admin"
+
     visit person_path(shane)
-    page.find("#flags a.btn-block").click
+
+    click_button "Flag @#{shane.username} as inappropriate"
+
+    visit person_path(shane)
+    click_link "Block @#{shane.username}"
+
     expect(page).to have_selector ".alert"
     expect(page).to have_selector ".unblock"
+
     visit "/admin"
-    click_link "Red Flags →"
-    # TODO FIXME
-    # page.find(".red-flag a").click
-    # click_button "Delete User"
-    # expect(current_path).to eq "/admin"
-    # visit person_path(shane)
-    # expect(current_path).to eq "/"
+    click_link "user-#{shane.id}"
+    click_button "Delete User"
+    expect(current_path).to eq "/admin"
+    visit person_path(shane)
+    expect(current_path).to eq "/"
   end
 end
