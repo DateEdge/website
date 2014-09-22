1c087deb649933a8f9ff6b1db4f2d46064e28ee0
Shane Becker <veganstraightedge@gmail.com>
2014-09-21 19:54:26 -0700

    Improve Red Flag Admin section. Part 1.

--- app/controllers/admin/red_flags_controller.rb
+++ app/controllers/admin/red_flags_controller.rb
@@ -1,6 +1,8 @@
 class Admin::RedFlagsController < AdminController
   def index
     @red_flags = RedFlag.group(:slug).count(:id).sort_by {|slug,count| count }.reverse
+    @flagged_users  = @red_flags.reject{ |r| r.first =~ /photo/ }
+    @flagged_photos = @red_flags.reject{ |r| r.first =~ /user/  }
   end
   
   def show
--- app/helpers/red_flags_helper.rb
+++ app/helpers/red_flags_helper.rb
@@ -1,5 +1,12 @@
 module RedFlagsHelper
 
+  def red_flag_classes(count)
+    classes = []
+    classes << "red-flag"
+    classes << "danger" if count > 1
+    classes.join(" ")
+  end
+
   def flagged?(obj)
     return unless current_user
     current_user.red_flag_reports.where(flaggable_id: obj.id, flaggable_type: obj.class.to_s).any?
