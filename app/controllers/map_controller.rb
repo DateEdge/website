b22703bc2dc8c501c623a005f1ebf759cd9ef5ce
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-02 22:27:44 -0800

    make markers only show active adults

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,6 @@ class MapController < ApplicationController
   def index
     @slug = "map-container"
     @lat_lng = LatLng.find_by(user_id: current_user.id) if current_user
+    @lat_lngs = LatLng.adults.visible
   end
 end
