ed2cf370b65bed881d111341b0b671389c168260
Bookis Smuin <bookis.smuin@gmail.com>
2014-11-09 22:36:00 -0800

    rearrange redis config to not use global var

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,7 @@
-if ENV["REDISCLOUD_URL"]
+if Rails.env.production?
   uri = URI.parse(ENV["REDISCLOUD_URL"])
-  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-  Resque.redis = $redis
+  redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Resque.redis = redis
+else
+  Resque.redis = Redis.new
 end
