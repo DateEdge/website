4a96fde818b7ddee7172301b00535ef2c88ccd02
Bookis Smuin <bookis.smuin@gmail.com>
2014-12-31 22:28:31 -0800

    make notification job send an email

--- app/jobs/notification_job.rb
+++ app/jobs/notification_job.rb
@@ -1,7 +1,8 @@
 class NotificationJob
   @queue = :notification
 
-  def self.perform(user_id)
-
+  def self.perform(notification_type, *args)
+    Notification.send(notification_type, *args).deliver
   end
+
 end
