0cc3fbc275b328ba28f80076d6dea990c05bea04
Shane Becker <veganstraightedge@gmail.com>
2015-01-03 21:11:57 -0800

    Write copy for new crush/message emails

--- app/mailers/notification.rb
+++ app/mailers/notification.rb
@@ -2,16 +2,16 @@ class Notification < ActionMailer::Base
   default from: "us@dateedge.com"
 
   def new_message(message_id)
-    @message = Message.find(message_id)
+    @message   = Message.find(message_id)
     @recipient = @message.recipient
     @sender    = @message.sender
-    mail to: @recipient.email, subject: "You have a new message from #{@sender.username}"
+    mail to: @recipient.email, subject: "Date Edge: You have a new message from @#{@sender.username}"
   end
 
   def new_crush(crush_id)
-    @crush = Crush.find(crush_id)
+    @crush     = Crush.find(crush_id)
     @recipient = @crush.crushee
     @sender    = @crush.crusher
-    mail to: @recipient.email, subject: "You were crushed on by #{@sender.username}"
+    mail to: @recipient.email, subject: "Date Edge: You were crushed on by @#{@sender.username}"
   end
 end
