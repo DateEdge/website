182b341a56b9283c5c1f82bb3956b2f1400f8b71
Bookis Smuin <bookis.smuin@gmail.com>
2016-03-27 07:47:33 -0700

    Change to use SES from Mandrill

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,9 @@
 unless Rails.env.test?
   ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
+    :port =>           '25',
+    :address =>        'email-smtp.us-west-2.amazonaws.com',
+    :user_name =>      ENV['SMTP_USERNAME'],
+    :password =>       ENV['SMTP_PASSWORD'],
     :domain =>         'dateedge.com',
     :authentication => :plain
   }
