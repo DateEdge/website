c7abe0377034bffd5c8fe8fd857cd37e609f95ba
Bookis Worthy <bookis.smuin@gmail.com>
2017-02-18 18:46:23 -0800

    Change session store setter to Rails.app...

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Dxe::Application.config.session_store :cookie_store, key: '_dateedge_session'
+Rails.application.config.session_store :cookie_store, key: '_dateedge_session'
